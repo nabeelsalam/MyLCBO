301cf19f095592429340e3b0b7907eb6f72cd358
nabeelsalam <nabeelsalam00@gmail.com>
2018-09-21 16:37:56 -0400

    Stock based on store

--- server.js
+++ server.js
@@ -3,24 +3,64 @@ const express = require('express'),
       cors = require('cors'),
       request = require('request'),
       BASE_URL='http://lcboapi.com',
-      API_KEY='MDplMGY4ZTBkYy1iYzIxLTExZTgtOTU3Zi0xYmRjOWEzMWZmNzk6VE43QWZmYUZCRXRFazcwMXZKWnFBNEhzYTREVVJrYW92V3BG'
+      API_KEY='MDplMGY4ZTBkYy1iYzIxLTExZTgtOTU3Zi0xYmRjOWEzMWZmNzk6VE43QWZmYUZCRXRFazcwMXZKWnFBNEhzYTREVVJrYW92V3BG',
+      bodyParser = require('body-parser');
 
-// Enable cross-origin resource sharing.
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8005, () => {
   console.log('Server Listening!')
 })
 
-app.use('/products', (req, res) => {
+app.get('/products', (req, res) => {
     let url =  `${BASE_URL}/products?access_key=${API_KEY}`;
-    if(req.query.searchText){
+    if(req.query.searchText && req.query.searchText.length > 0){
         url+=`&q=${req.query.searchText}`;
+    }
+    if(req.query.store && req.query.store != 0){
+        url+=`&store_id=${req.query.store}`;
     }
     console.log('Received hit: '+url)
   request(url, (error, response, body) => {
     res.send(body)
   })
 })
+
+app.get('/stores', (req, res) => {
+    let url =  `${BASE_URL}/stores?per_page=50&access_key=${API_KEY}`;
+    if(req.query.searchText){
+        url+=`&geo=${req.query.searchText}`;
+    }
+    console.log('Received hit: '+url)
+  request(url, (error, response, body) => {
+    res.send(body)
+  })
+})
+
+app.post('/stock', function(req, res) {
+    const ids = req.body.productIds;
+    let promises = [];
+    ids.forEach(id => {
+        let url =  `${BASE_URL}/stores/${req.body.store}/products/${id}/inventory?access_key=${API_KEY}`;
+        promises.push(new Promise((resolve)=>{
+            request(url, (error, response, body) => {
+                resolve(body);
+            })
+        }))
+    });
+    Promise.all(promises)
+        .then((results) => {
+            console.log(results)
+            let stockObject = {}
+            results.forEach((el,index) => {
+                let key = ids[index];
+                stockObject[key] = JSON.parse(el).result;               
+            });
+            res.send(stockObject); 
+        })
+        .catch(err => console.log(err));
+});
 
 
--- src/App.js
+++ src/App.js
@@ -10,15 +10,49 @@ import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import CardActions from '@material-ui/core/CardActions';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import Badge from '@material-ui/core/Badge';
 
 import './App.css';
 
 const URLs={
   base: 'http://localhost:8005/',
-  products: 'products'
+  products: 'products',
+  stores: 'stores',
+  stock: 'stock'
 }
 
 const HeaderComponent = props => {
+  let storeSelector = '';
+  if(props.stores.length > 0 ){
+    storeSelector =   <form className="" autoComplete="off">
+    <FormControl className="formControl">
+      <InputLabel htmlFor="age-simple">Your Store</InputLabel>
+      <Select
+        value={props.selectedStore}
+        onChange={props.onStoreChange}
+        inputProps={{
+          name: 'age',
+          id: 'age-simple',
+        }}
+      >
+      <MenuItem value="">
+          <em>None</em>
+      </MenuItem>
+      {
+        props.stores.map(store => (
+        <MenuItem value={store.id}>{store.name} - ({store.address_line_1})</MenuItem>          
+        ))
+      }
+      </Select>
+    </FormControl>
+  </form>;
+  }
+
+
   return (
     <AppBar position="fixed" color="default">
     <Toolbar>
@@ -38,7 +72,9 @@ const HeaderComponent = props => {
           shrink: true,
         }}
         onChange={props.onChange}
-    />
+      />
+      {storeSelector}
+
     </Toolbar>
   </AppBar> 
   )
@@ -78,6 +114,15 @@ const ProductListComponent = (props) => {
                 <Typography variant="subheading" gutterBottom>
                   ({card.package})
                 </Typography>
+                {
+                  card.stock && card.stock.quantity && (
+                    <Badge className="badge" color="primary" badgeContent={card.stock.quantity}>
+                    <Typography variant="button" gutterBottom>
+                      Stock
+                    </Typography>
+                    </Badge>
+                  )
+                }
                 </CardActions>
               </Card>
             </Grid>
@@ -92,27 +137,71 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      products: []
+      products: [],
+      stores: [],
+      selectedStore: 0,
+      searchTerm: ''
     };
   }
 
   componentDidMount() {
-    fetch(`${URLs.base}${URLs.products}`)
+    this.getProducts();
+    this.getStores();
+  }
+
+  getStores(){
+    fetch(`${URLs.base}${URLs.stores}`)
       .then(response => response.json())
-      .then(data => this.setState({ products: data.result }))
+      .then(data => this.setState({ stores: data.result }))
       .catch(err => console.log(err));
   }
 
-  handleChange = event => {
-    fetch(`${URLs.base}${URLs.products}?searchText=${event.target.value}`)
-      .then(response => response.json())
-      .then(data => this.setState({ products: data.result }))
-      .catch(err => console.log(err));  
+  getProducts(){
+    fetch(`${URLs.base}${URLs.products}?searchText=${this.state.searchTerm}&store=${this.state.selectedStore}`)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({ products: data.result });
+      this.getStock();
+    })
+    .catch(err => console.log(err));  
+  }
+
+  getStock(){
+    const productIds = this.state.products.map(product => product.id);
+    fetch(`${URLs.base}${URLs.stock}`, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        store: this.state.selectedStore,
+        productIds
+      })
+    }).then(res=>res.json())
+      .then(stock => {
+        let {products} = this.state;
+        products.map(product => {
+          product.stock = stock[product.id]
+        })
+        this.setState({ products })
+      });
+  }
+
+  handleSearch = event => {
+    this.setState({ searchTerm: event.target.value });
+    this.getProducts();
+  }
+
+  handleStoreChange = event => {
+    this.setState({ selectedStore: event.target.value });
+    this.getProducts();
   }
 
   render() {
-    const {products} = this.state;
+    const {products, stores, selectedStore } = this.state;
     let body;
+
     if(products.length > 0){
       body = <ProductListComponent products={products}></ProductListComponent>;
     }
@@ -121,7 +210,12 @@ class App extends Component {
     }
     return (
       <div>
-        <HeaderComponent onChange={this.handleChange}></HeaderComponent>
+        <HeaderComponent 
+          stores={stores} 
+          selectedStore = {selectedStore} 
+          onStoreChange={this.handleStoreChange}
+          onChange={this.handleSearch}>
+        </HeaderComponent>
         {body}
     </div> 
     );
